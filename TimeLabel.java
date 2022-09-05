bb30488c270e2befe4eb4bd96ef327c96abb8c97
Jeff Manning <info@jeffmanning.ca>
2022-09-04 20:27:40 -0400

    incrementing score

--- TimeLabel.java
+++ TimeLabel.java
@@ -6,13 +6,18 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
 
 
 public class TimeLabel extends Label
 {
-
+    private Timer gameTimer;
+    private int timeElapsed; 
     public TimeLabel(String value) {
         super(value);
+        timeElapsed = 0;
         
         try {
             InputStream fontStream = TimeLabel.class.getResourceAsStream("assets/digitaldream.ttf");
@@ -25,9 +30,26 @@ public class TimeLabel extends Label
         catch (java.io.IOException ioe) {
             // the font isn't critical to operation so we're going to let is fail gracefully and use a sys font.
         }
-        
+
         setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
         setTextFill(Color.RED);
         setPadding(new Insets(5,10,10,10));
+        startTimer();
+    }
+
+    public void startTimer() {
+        gameTimer = new Timer();
+        TimerTask incrementClock = new TimerTask() {
+                public void run() {
+                    System.out.println("run");
+                    timeElapsed++;
+                    Platform.runLater(new Runnable() {
+                        @Override public void run() {
+                            setText(Integer.toString(timeElapsed));
+                        }
+                    });
+                } 
+            };
+        gameTimer.schedule(incrementClock,0, 1000);
     }
 }
